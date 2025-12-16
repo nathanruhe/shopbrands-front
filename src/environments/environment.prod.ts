aee0c5a6667c104558ecbde52e3a9734ff78629f
nathanruhe <nathan.ruhe@hotmail.com>
2025-12-16 02:10:13 +0100

    corrección environments, se elimina .env#

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://tu-dominio.com/api'
+    apiUrl: 'https://tu-dominio.com/api',
+    STRIPE_PUBLIC_KEY: '',
+    DEFAULT_LANGUAGE: 'es',
+    APP_NAME: 'ShopProject'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,7 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:3000/api'
+    apiUrl: 'http://localhost:3000/api',
+    STRIPE_PUBLIC_KEY: 'pk_live_51SL3h5HuoBKcHqdrrJEvwvI4UbgS2G5MFXOAYw1a9HDiWr3GzaWlKhbviAkVAexcFqb8YcTNWdUC6J7blgqd7x5700mpTZiwso',
+    DEFAULT_LANGUAGE: 'es',
+    APP_NAME: 'ShopProject'
 };
